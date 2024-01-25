7d0af281866917b303a2d2c13c1cb38a9b7bc4d3
mirbekovaa76 <mirbekovaa76@gmail.com>
2024-01-25 09:42:07 +0600

    Revert "C1-commit-3"
    This reverts commit 33cc0808aba5dc8904a90ec0c4f0df42172406ef.

--- src/C/C1.java
+++ src/C/C1.java
@@ -1,11 +1,4 @@
 package C;
 
 public class C1 {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-
-    public static void sayHello(){
-        System.out.println("Hello");
-    }
 }
