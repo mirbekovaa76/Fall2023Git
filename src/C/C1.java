33cc0808aba5dc8904a90ec0c4f0df42172406ef
mirbekovaa76 <mirbekovaa76@gmail.com>
2024-01-25 09:40:25 +0600

    C1-commit-3

--- src/C/C1.java
+++ src/C/C1.java
@@ -1,4 +1,11 @@
 package C;
 
 public class C1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
 }
