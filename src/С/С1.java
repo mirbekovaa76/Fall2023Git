d8baf9f2f4d05084bc104af693478e9ee90e1b25
mirbekovaa76 <mirbekovaa76@gmail.com>
2024-01-26 16:23:48 +0600

    C1-commit-1

--- src/С/С1.java
+++ src/С/С1.java
@@ -4,4 +4,8 @@ public class С1 {
     public static void main(String[] args) {
         System.out.println("C1 class");
     }
+
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
 }
